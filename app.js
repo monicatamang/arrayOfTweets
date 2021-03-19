a739a0ddedfb0ede420b3f39b17ff625ab61454e
Monica <tamang.monica@gmail.com>
2021-03-19 12:05:50 -0600

    Created an array of strings, created a for loop that prints each tweet to the console

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initializing an array of 11 tweets as strings
+var tweets = ["Thanks is short for Tom Hanks", "I hope this email finds you lost at sea", "People watching is my favourite hobby", "Yogurt expires exactly two days before you decide you want it. It's science.", "I was born at a very young age", "I hope the all pigeons have a great day today", "I just read '22nd' as 'secondly second' in my head just now", "There isn't a single virgo who believes in astrology", "Who called it a subtweet instead of a tea sis statement", "Why is it called creepypasta and not fettuccino afraido", "Every movie trilogy is a Christmas movie"];
+
+// Storing the length of the array into a new variable
+var tweetsLength = tweets.length;
+
+// Writing a for loop conditional that will print each tweet in the array one by one to the console
+for (i = 0; i < tweetsLength; i++) {
+    console.log(tweets[i]);
+}
